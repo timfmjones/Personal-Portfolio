88e064c8cd7c143d2aafb989b688a5c20c21514e
timfmjones <tfmjones@uwaterloo.ca>
2024-06-18 21:44:28 -0400

    fix routing

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
       <div className="App">
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/Personal-Portfolio/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/skills" element={<SkillsPage />} />
