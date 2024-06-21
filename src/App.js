a73d35fc2d0840ee1887e4a66b015bfdc9d977e7
timfmjones <tfmjones@uwaterloo.ca>
2024-06-20 22:07:13 -0400

    route unknown to homepage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
@@ -24,6 +24,7 @@ function App() {
           <Route path="/projects" element={<Projects />} />
           <Route path="/skills" element={<SkillsPage />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
         <BackToTopButton />
       </div>
